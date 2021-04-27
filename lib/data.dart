2824accef104ffc5e0768fc9e4b7ba3af2b23177
Your Name <nirmitjatana38@gmail.com>
2021-04-27 22:27:12 +0530

    feat: data file to store alarms

---
+++ lib/data.dart
@@ -0,0 +1,17 @@
+import 'models/alarminfo.dart';
+
+List<AlarmInfo> alarms = [
+  AlarmInfo(DateTime.now().add(Duration(hours: 1)), description: 'Yoga'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+  AlarmInfo(DateTime.now().add(Duration(hours: 2)), description: 'oFFICE'),
+];
